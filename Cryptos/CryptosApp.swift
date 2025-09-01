52532795d14edd6768b0265b498a092772b485e4
Gustavo Freitas Rosa <heygu95@gmail.com>
2025-09-01 18:50:31 +0100

    Setup project

---
+++ Cryptos/CryptosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptosApp.swift
+//  Cryptos
+//
+//  Created by Gustavo Freitas on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptosTests/CryptosTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptosTests.swift
+//  CryptosTests
+//
+//  Created by Gustavo Freitas on 01/09/2025.
+//
+
+import Testing
+@testable import Cryptos
+
+struct CryptosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
